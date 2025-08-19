0ad595497ba463f8e88e3be9c93714c78f79e17c
nesasheeba38 <nesasheebaj32@gmail.com>
2025-08-19 21:33:24 +0530

    intergrated MFE plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+   federation({
+            name: 'host-app',
+            remotes: {
+                uistudent: "http://localhost:5174/remoteEntry.js",
+            },
+            shared: ['react','react-dom']
+        })  
+  ],
+  build:{
+    target:'esnext'
+  }
 })
