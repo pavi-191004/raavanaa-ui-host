bd1cb1ae1baf3c85d3de35dc18c825443b16bb20
Mentor Bridge <mentorbridgeindia@gmail.com>
2025-08-19 23:05:16 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-   federation({
-            name: 'host-app',
-            remotes: {
-                uistudent: "http://localhost:5174/remoteEntry.js",
-            },
-            shared: ['react','react-dom']
-        })  
+  plugins: [
+    react(),
+    federation({
+      name: "host-app",
+      remotes: {
+        UIComponents: "http://localhost:5174/assets/remoteEntry.js",
+      },
+      shared: ["react", "react-dom"],
+    }),
   ],
-  build:{
-    target:'esnext'
-  }
-})
+  build: {
+    target: "esnext",
+  },
+});
