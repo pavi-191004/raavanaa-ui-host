ffed5464bcd6752455d7b77478af4600cb84a648
Mentor Bridge <mentorbridgeindia@gmail.com>
2025-08-19 23:05:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
+import React from "react";
+import "./App.css";
+const RemoteApp = React.lazy(() => import("UIComponents/App"));
+
+const Button = React.lazy(() => import("UIComponents/Button"));
 
 function App() {
-return (
-      <div>
-        <h1>Welcome to Host Module</h1>
-      </div>
-       )
+  return (
+    <div>
+      <h1>Welcome to Host Module</h1>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <RemoteApp />
+        <Button />
+      </React.Suspense>
+    </div>
+  );
 }
 
-export default App
+export default App;
