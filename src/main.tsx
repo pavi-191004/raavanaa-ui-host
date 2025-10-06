a5204fa361f0ae62a91e3697f28574e497d664bc
KethraPackiamB <kethrapackiamb@gmail.com>
2025-10-06 18:09:04 +0530

    Created the Student Staff selection card page

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.min.css';
 // import "./index.css";
 import App from "./App.tsx";
-import { RegisterApp } from "./Raavana-RegisterPage/RegisterApp.tsx";
+
 import { RoutingPages } from "./StudentStaffSelectionPage/RoutingPages.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <RegisterApp /> */}
+    
     <RoutingPages/>
     
   </StrictMode>
