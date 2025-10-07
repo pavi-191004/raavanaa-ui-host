7b25d8e33e8475df54394c4ee9be16fa3d7c688b
nesasheeba38 <nesasheebaj32@gmail.com>
2025-10-07 21:40:15 +0530

    updated Login Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 import React from "react";
-import "./App.css";
-import { AuthKitProvider } from '@workos-inc/authkit-react';
-import { LoginPage } from './Raavanaa-Login-Page/LoginPage';
+// import "./App.css";
+const RemoteApp = React.lazy(() => import("UIComponents/App"));
+
+const Button = React.lazy(() => import("UIComponents/Button"));
+
+const AdminApp = React.lazy(() => import("admin_fe/App"));
+
+const StudentApp = React.lazy(() => import("ui_student/App"));
+
+const RecruiterApp = React.lazy(() => import("raavana_ui_recruiter/App"));
 
 function App() {
   return (
-     <div>
-         <AuthKitProvider
-             clientId="client_01K54E5FHK9CE850VVJ3X864X7">
-            <LoginPage/>
-          </AuthKitProvider>
-        </div>
+    <div>
+      <h1>Welcome to Host Module</h1>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <RemoteApp />
+        <Button />
+        <AdminApp />
+        <StudentApp />
+        <RecruiterApp />
+      </React.Suspense>
+    </div>
   );
 }
 
